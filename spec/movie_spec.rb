24523ce3780416ab375201fea5beef0a0f0918af
Donny <donny.hyon@madetech.com>
2022-06-07 14:58:59 +0100

    first test running

---
+++ spec/movie_spec.rb
@@ -0,0 +1,24 @@
+=begin
+Genre
+- Horror
+    -Psychological / killer
+        -Home invasion & survival
+- Villain
+    - single person
+    -background of trauma
+        - motivation for actions is targeting those who hurt them in the past
+
+- Time setting
+    - birthday of murderer 1/ 1/ 1970 (52)
+    - child hood    
+=end
+
+describe "my movie" do
+    it "is it a horror?" do
+        genre = "comedy"
+        expect(genre).to eq('horror')
+    end
+
+    it "is a "
+
+end
